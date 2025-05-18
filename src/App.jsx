9a380dd0e17c498f2970068359a5d82eef5723a7
Cosimo0C <cosimocrupi01@gmail.com>
2025-05-18 17:10:20 +0200

    S9-L2

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,15 @@ import CustomNavbar from "./component/CustomNavbar";
 import MyFooter from "./component/MyFooter";
 import Welcome from "./component/Welcome";
 import AllTheBooks from "./component/AllTheBooks";
+import { Container } from "react-bootstrap";
 function App() {
   return (
-    <>
+    <Container fluid>
       <CustomNavbar />
       <Welcome />
-      <div className="container">
-        <AllTheBooks />
-      </div>
+      <AllTheBooks />
       <MyFooter />
-    </>
+    </Container>
   );
 }
 
